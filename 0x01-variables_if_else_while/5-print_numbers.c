6a3c68ab11d64f137fb0130205245a1626d78f17
LillianKamanga <lilliankamanga3@gmail.com>
2023-06-18 13:39:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - demonstrates the usage of the puts() function.
+ *@void: no parameters accepted
+ *It prints a string to the console.
+ *Return: 0  Program executed successfully.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf(a);
+	putchar('\n');
+
+	return (0);
+}
