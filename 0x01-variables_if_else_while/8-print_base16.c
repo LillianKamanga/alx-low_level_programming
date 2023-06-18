0d59dff4c9737613c6dabc287a6390e722196175
LillianKamanga <lilliankamanga3@gmail.com>
2023-06-18 14:06:13 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - point of execution
+ *@void: no parameters accepted
+ *Tells you whether a number is negative, positive or zeroe
+ *Return: 0  Program executed successfully.
+ */
+int main(void)
+{
+	char c, p;
+
+	for (c = '0'; c <= '9'; c++)
+	putchar(c);
+	for (p = 'a'; p <= 'f'; p++)
+        putchar(p);
+	putchar('\n');
+
+return (0);
+}
