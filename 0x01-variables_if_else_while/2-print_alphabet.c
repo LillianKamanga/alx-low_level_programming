ea230d62d3a07f85deed77023966231fe3531c37
LillianKamanga <lilliankamanga3@gmail.com>
2023-06-18 12:50:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - point of execution
+ *@void: no parameters accepted
+ *Tells you whether a number is negative, positive or zeroe
+ *Return: 0  Program executed successfully.
+ */
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++);
+	putchar(c);
+	putchar('\n');
+
+return (0);
+
+}
+
