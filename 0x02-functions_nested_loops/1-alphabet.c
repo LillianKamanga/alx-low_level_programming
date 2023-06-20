cfaafafbaeedc160f261da356aaaf4742d552d9b
LillianKamanga <lilliankamanga3@gmail.com>
2023-06-20 11:30:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	void print_alphabet(void);
+	{
+
+	for (int i = 'a' ; i <= 'z' ; i++)
+	putchar('a');
+	putchar('\n');
+	}
+
+return (0);
+
+
+}
